52e5f8b12056fa689889c1c6762f87ac4fb89e21
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-04-03 14:15:02 +0530

    style: profile (client)

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -29,19 +29,21 @@ const tabs = [
 ]
 
 function Profile() {
-  const currentUser = useSelector( store => store.profile.userData )
+  const user = useSelector( store => store.profile.userData )
 
   return (
     <div className='p-10'>
       <div className="user-details grid grid-cols-4 gap-5">
         <div className="col-span-1 flex-center">
-            <img src="https://i.pinimg.com/736x/60/72/cb/6072cb81fbaac9f7750561675ef9ad71.jpg" alt="profile" className='rounded p-1 border'/>
+            <img src={user?.image || "/images/profile/unknown.jpg"} alt="profile" className='rounded p-2 border'/>
         </div>
         <div className="col-span-3 p-2">
-          <h1 className='font-bold text-xl'>{currentUser?.name}</h1>
-          <h2 className='font-medium text-gray-400'>{currentUser?.role}</h2>
-          <h2 className='flex items-center'><FaMapMarkerAlt /> Chennai</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolor dolore ad reprehenderit adipisci aliquam accusamus repellendus sunt perspiciatis voluptatibus sit quaerat omnis, qui dolorum assumenda consequuntur aliquid veritatis nulla.</p>
+          <h1 className='font-bold text-xl'>{user?.name}</h1>
+          <h2 className='font-medium text-gray-400'>{user?.role}</h2>
+          <h2 className='flex items-center'><FaMapMarkerAlt/>{user?.location}</h2>
+          <p>
+            {user?.bio}
+          </p>
           <ProfileModal />
         </div>
       </div>
