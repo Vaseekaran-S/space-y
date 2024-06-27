4f5aae376c359e9c1855181c9c4e8c0ec17efedb
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-06-27 22:25:33 +0530

    test: removing lazy component for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { verifyToken } from './api/auth';
 import { getUser } from './api/users';
 
-const Home = React.lazy(() => import('./pages/Home'));
-const Login = React.lazy(() => import('./pages/Login'));
-const SignUp = React.lazy(() => import('./pages/SignUp'));
-const Profile = React.lazy(() => import('./pages/Profile'));
-const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Home = import('./pages/Home');
+const Login = import('./pages/Login');
+const SignUp = import('./pages/SignUp');
+const Profile = import('./pages/Profile');
+const NotFound = import('./pages/NotFound');
 
 export default function App() {
 
